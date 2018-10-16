fe6e868645d71e56c779e1790b410495601e0e6a
douhab touly <douhabtouly1994@outlook.com>
2018-10-16 17:11:38 +0100

    git all link of the page

--- python.py
+++ python.py
@@ -1,1 +1,8 @@
-print 2+2
+def git_next_targit(page) :
+    start_link=page.find('<a href=')
+    if start_link==-1:
+        return None,0
+    stat_quote=page.find('"',start_page)
+    end_quot=page.find('"',statr_quot+1)
+    url=page[start_quot+1:end_quote]
+    return url,end_quote
